4b92d6d45eb441904bf56fc5c1e057275b947aab
VitoCostello1 <98420235+VitoCostello1@users.noreply.github.com>
2022-03-31 11:59:12 -0400

    Inital commit

---
+++ script9.js
@@ -0,0 +1,9 @@
+// script for Ch.9 practice project
+"use strict";
+
+document.getElementById("bottom").textContent = "parappa > funkin";
+document.getElementsByClassName("main")[1].innerHTML = "pain without love! <br/> <h3>pain can't get enough.</h3>";
+
+
+document.getElementsByTagName("h1") [0].style.fontSize = "40px";
+document.getElementsByTagName("h1") [0].style.color= "green";
